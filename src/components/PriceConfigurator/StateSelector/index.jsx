06642f74bd35e61ba70ea7dcdbb8dc3af0564e3e
Leandro Oriente <leandroriente@gmail.com>
2016-08-07 19:49:50 -0300

    Change state selector to stateless

--- src/components/PriceConfigurator/StateSelector/index.jsx
+++ src/components/PriceConfigurator/StateSelector/index.jsx
@@ -2,34 +2,36 @@ import React, { Component, PropTypes } from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-class StateSelector extends Component {
-  handleChange = (event, index, value) => {
-    this.props.selectState(value);
+const StateSelector = ({list, selected, isFetching, selectState}) => {
+  const handleChange = (event, index, value) => {
+    selectState(value);
   }
 
-  render() {
-    const { list, selected, isFetching } = this.props;
+  const getItems = (list) => {
+    const items = [];
 
-    const items = []
     for (let i = 0; i < list.length; i++) {
       const value = list[i];
       items.push(<MenuItem key={i} value={value} primaryText={value} />)
     }
 
-    return (
-      <SelectField
-        id="state-select"
-        disabled={isFetching}
-        maxHeight={200}
-        style={{width: '100%', marginBottom: 24}}
-        value={selected}
-        onChange={this.handleChange}
-        floatingLabelText="Selecione o seu estado">
-      {items}
-      </SelectField>
-    )
+    return items;
   }
+
+  return (
+    <SelectField
+      id="state-select"
+      disabled={isFetching}
+      maxHeight={200}
+      style={{width: '100%', marginBottom: 24}}
+      value={selected}
+      onChange={handleChange}
+      floatingLabelText="Selecione o seu estado">
+    {getItems(list)}
+    </SelectField>
+  )
 }
+
 
 StateSelector.propTypes = {
   selectState: PropTypes.func.isRequired,
