89250cc0ab9196c1808436d013cf08df61abe110
Leandro Oriente <leandroriente@gmail.com>
2016-08-07 04:35:06 -0300

    Add Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => (
+  <header className="mdl-layout__header">
+    <h1 className="mdl-layout-title mdl-layout-title__centered">Comprar ou alugar?</h1>
+  </header>
+);
+
+export default Header;
