44e35b20bc9e77a88a029863b2d5ab8e37b7ee79
Leandro Oriente <leandroriente@gmail.com>
2016-08-09 05:19:05 -0300

    Add dynamic background component

---
+++ src/components/BackgroundImages/index.jsx
@@ -0,0 +1,79 @@
+import React, { PropTypes, Component } from 'react';
+import { sample } from 'lodash';
+import getImages from 'actions/images';
+import './style.scss';
+
+const propTypes = {
+  children: PropTypes.element,
+};
+
+class BackgroundImages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      slideshow: false,
+      selected: null,
+      images: [],
+    };
+  }
+
+  componentWillMount() {
+    getImages()
+      .then(images => {
+        const mobile = window.outerWidth < 840;
+        const responsiveImages = images.map(i => {
+          const url = i[mobile ? 'small' : 'large'];
+          const img = document.createElement('img');
+          img.src = url;
+
+          return url;
+        });
+
+        this.setState({
+          slideshow: true,
+          selected: sample(responsiveImages),
+          images: responsiveImages,
+        });
+      });
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (!this.state.selected && nextState.selected) {
+      const { selected, images } = nextState;
+      this.timer = setInterval(() => {
+        this.setState({ selected: this.getSampleImage(selected, images) });
+      }, 25 * 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  getSampleImage(selected, images) {
+    const sortedImage = sample(images);
+    if (sortedImage !== selected) return sortedImage;
+
+    return this.getSampleImage(selected, images);
+  }
+
+  render() {
+    if (this.state.selected === null) {
+      return <div>{this.props.children}</div>;
+    }
+
+    return (
+      <div
+        className="background-cover"
+        style={{ backgroundImage: `url('${this.state.selected}')` }}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+BackgroundImages.propTypes = propTypes;
+
+export default BackgroundImages;
