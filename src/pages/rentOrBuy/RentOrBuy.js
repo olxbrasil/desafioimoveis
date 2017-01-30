7aadcb41b0b8faf7539846ad9fa8e51ad73c8e3c
Vitor Mattos <vitor.mattos@vtex.com.br>
2017-01-30 14:20:39 -0200

    Initial structure

---
+++ src/pages/rentOrBuy/RentOrBuy.js
@@ -0,0 +1,35 @@
+
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import fetch from '../../core/fetch';
+import history from '../../core/history';
+
+class RentOrBuy extends React.Component {
+
+  render() {
+    return(
+      <h1>Teste</h1>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+});
+
+RentOrBuy.propTypes = {
+}
+
+RentOrBuy.contextTypes = {
+  store: PropTypes.object,
+}
+
+export default connect(mapStateToProps)(RentOrBuy);
---
+++ src/pages/rentOrBuy/index.js
@@ -0,0 +1,27 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import fetch from '../../core/fetch';
+import Layout from '../../components/Layout';
+import RentOrBuy from './RentOrBuy';
+
+export default {
+
+  path: '/',
+
+  async action() {
+
+    return {
+      title: 'desai',
+      component: <Layout><RentOrBuy/></Layout>,
+    }
+  },
+
+};
