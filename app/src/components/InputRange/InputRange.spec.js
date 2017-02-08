928fc934a720f3df963924a22c23ecf75ce54f4b
Alan Gomes <alanhr2@gmail.com>
2017-02-08 08:08:10 -0200

    change test because ad new props prefix and sifix inside of InputRange

--- app/src/components/InputRange/InputRange.spec.js
+++ app/src/components/InputRange/InputRange.spec.js
@@ -10,6 +10,8 @@ const props = {
 	defaultValue: '0',
 	step: '0',
 	name: 'teste',
+	prefix: '',
+	sufix: '',
 };
 
 describe('<InputRange />', () => {
@@ -26,49 +28,46 @@ describe('<InputRange />', () => {
 	it('Should have teste inside of Label', () => {
 		const wrapper = shallow(<InputRange {...props} />);
 		const label = wrapper.find('label');
-		expect(label).to.have.text('Teste 0');
+		expect(label).to.have.text('Teste');
 	});
 
-	it('Should have teste inside of complex Label', () => {
-		props.label = { before: 'Teste', after: 'GG' };
-		const wrapper = shallow(<InputRange {...props} />);
-		const label = wrapper.find('label');
-		expect(label).to.have.text('Teste 0 GG');
-	});
-
-	it('Should have teste inside of complex Label with number format', () => {
-		props.label = { before: 'Teste', after: 'GG' };
+	it('Should have teste mask with number format', () => {
 		props.defaultValue = '300';
 		props.formatNumber = '0,0[.]00';
 		const wrapper = shallow(<InputRange {...props} />);
-		const label = wrapper.find('label');
-		expect(label).to.have.text('Teste 300 GG');
+		const div = wrapper.find('.input-range__display');
+		expect(div).to.have.text(' 300 ');
 	});
 
-	it('Should have teste inside of complex Label with number format Custom Percente', () => {
-		props.label = { before: 'Teste', after: 'GG' };
+	it('Should have teste mask with number format using prefix and sufix', () => {
+		props.defaultValue = '300';
+		props.formatNumber = '0,0[.]00';
+		const wrapper = shallow(<InputRange {...props} prefix="R$" sufix="anos" />);
+		const div = wrapper.find('.input-range__display');
+		expect(div).to.have.text('R$ 300 anos');
+	});
+
+	it('Should have teste mask  with number format Custom Percente', () => {
 		props.defaultValue = '25';
 		props.formatNumber = '0cp';
 		const wrapper = shallow(<InputRange {...props} />);
-		const label = wrapper.find('label');
-		expect(label).to.have.text('Teste 2.5% GG');
+		const div = wrapper.find('.input-range__display');
+		expect(div).to.have.text(' 2.5% ');
 	});
 
 	it('Should have teste inside of defaultValue and formatNumber is undefined', () => {
-		props.label = { before: 'Teste', after: 'GG' };
 		props.defaultValue = undefined;
 		props.formatNumber = undefined;
 		const wrapper = shallow(<InputRange {...props} />);
-		const label = wrapper.find('label');
-		expect(label).to.have.text('Teste GG');
+		const div = wrapper.find('.input-range__display');
+		expect(div).to.have.text(' ');
 	});
 
 	it('Should have teste inside of defaultValue and formatNumber is undefined', () => {
-		props.label = { before: 'Teste', after: 'GG' };
 		props.defaultValue = undefined;
 		props.formatNumber = undefined;
 		const wrapper = shallow(<InputRange {...props} />);
-		wrapper.setProps({defaultValue:10});
+		wrapper.setProps({ defaultValue: 10 });
 		const input = wrapper.find('input');
 		expect(input).to.have.prop('value', '10');
 
