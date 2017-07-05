c5adc1c5d221cc8acf5c90ea43a7d14804347c12
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-07-05 07:47:45 -0300

    Implement StateSelector component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from 'components/Card'
+import StateSelector from 'components/StateSelector'
 
 const App = () => (
   <div className="flex justify-center sans-serif">
     <Card>
       <h1 className="fw6 f1 mt0 tc">Comprar ou Alugar?</h1>
+      <div className="mh5">
+        <StateSelector />
+      </div>
     </Card>
   </div>
 )
