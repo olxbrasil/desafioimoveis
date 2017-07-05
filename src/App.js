e805ddfa0bed1a4f9c4105106d8579e84df6e9af
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-07-05 05:12:37 -0300

    Add tachyons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const App = () => (
-  <div>
-    <h1>Hello World</h1>
+  <div className="flex justify-center">
+    <h1 className="fw6 f1">Hello World</h1>
   </div>
 )
 
