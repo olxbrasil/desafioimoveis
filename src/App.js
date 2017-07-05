78d0fa489530537506adb73b3911bc9ac9b9b712
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-07-05 06:52:13 -0300

    Implement Card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Card from 'components/Card'
 
 const App = () => (
-  <div className="flex justify-center">
-    <h1 className="fw6 f1">Hello World</h1>
+  <div className="flex justify-center sans-serif">
+    <Card>
+      <h1 className="fw6 f1 mt0 tc">Comprar ou Alugar?</h1>
+    </Card>
   </div>
 )
 
