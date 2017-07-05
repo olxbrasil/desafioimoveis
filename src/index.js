39d0b6cdbd9236b0b10822eeeebac79ac5ae1c1a
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-07-05 04:52:28 -0300

    Add entry point files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+import App from './App'
+
+const renderComponent = (Component) => {
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+renderComponent(App)
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App.js', () => {
+    const nextApp = require('./App.js').default
+    renderComponent(nextApp)
+  })
+}
