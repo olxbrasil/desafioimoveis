5ac29f0f53fb4311a98c59730221f99c60f6b6a2
Thor Amorim <thoramorim@gmail.com>
2017-07-21 06:47:18 -0300

    Make Header title always align on the left

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header () {
   return (
     <header className="w-100 pv4 mb4">
-      <div className="w-90 w-80-l f2 fw6 black-90 center tc tl-l">
+      <div className="w-90 w-80-l f2 fw6 black-90 center">
         Alugar ou Comprar?
       </div>
     </header>
