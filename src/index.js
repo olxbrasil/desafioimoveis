3ce13be0ac246980d9c86ccd144b5879ff6de0a8
Thor Amorim <thoramorim@gmail.com>
2017-07-21 05:21:26 -0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import {render} from 'react-dom'
+import logger from 'redux-logger'
+import {Provider} from 'react-redux'
 import {AppContainer} from 'react-hot-loader'
+import {createStore, applyMiddleware} from 'redux'
 
 import App from './containers/App'
+import reducer from './redux/reducer'
 
-const renderApp = () =>
+const store = createStore(reducer, applyMiddleware(logger))
+
+const renderApp = Root =>
   render(
     <AppContainer>
-      <App />
+      <Provider store={store}>
+        <Root />
+      </Provider>
     </AppContainer>,
     document.getElementById('root')
   )
 
-renderApp()
+renderApp(App)
 
 if (module.hot) {
-  module.hot.accept('./containers/App', renderApp)
+  module.hot.accept('./containers/App', () => {
+    const Root = require('./containers/App').default
+    renderApp(Root)
+  })
 }
