c17cee96fb0316a13c42c4a66e46e4ae50cc434e
Gabriel Godoy <hi@gabrielgodoy.com>
2017-09-16 18:35:11 -0300

    Add images and scoped style support

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Title.scss';
 
 const Title = props => (
-  <h2 className={`title ${props.extraClasses}`}>{props.text}</h2>
+  <h2 styleName={`title ${props.extraClasses}`}>{props.text}</h2>
 );
 
 Title.propTypes = {
