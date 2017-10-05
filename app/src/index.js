667cfd4a76db5bff5a81c0db926ab621a67c017e
Andre <almoreira@mongeralaegon.com.br>
2017-10-05 13:59:11 -0300

    chore(Config): finishes basic configuration

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './reset.scss';
+import Home from '../views/home/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Home />, document.getElementById('root'));
