08f3e3e9d194cbc84594fa10e8d989ea2310ff50
Andre <andre.lom20@outlook.com>
2017-10-06 08:16:25 -0300

    feat(TypeScript): adds and finishes typescript configuration

---
+++ app/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './assets/scss/reset.scss';
+import Home from './views/home/Home';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root') as HTMLElement
+);
