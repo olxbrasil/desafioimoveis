8b5d83cec3770790547a522022974dea4734e2a2
Bruno Santana <brunosantana@MacBook-Pro-de-Bruno.local>
2017-10-07 03:58:48 -0300

    feat(getDataService): rendering states in the screen

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ import { bindActionCreators } from 'redux';
 import getStatesActions from './container/actions/HomeActions';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			actualState: 'AC',
+		}
+	}
 	componentWillMount() {
 		this.props.getStatesActions();
 	}
 	render() {
+		const { states } = this.props.home;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">{this.props.home.title}</h1>
 				</header>
+				<form>
+					<label>Selecione seu Estado</label>
+					<select	onChange={e => this.setState({actualState: e.target.value})}>
+						 {
+							states.map(e =>
+								<option key={e.id} value={e.state}>{e.state}</option>
+							)
+						 }
+					</select>
+				</form>
+				<h2>Estado atual: {this.state.actualState}</h2>
 			</div>
 		);
 	}
