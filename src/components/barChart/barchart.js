e9fb0bd82474a947c9710ccc2755d8fd148cccf5
Bruno Santana <brunosantana2santana@gmail.com>
2017-10-10 18:40:58 -0300

    feat(graphic)

--- src/components/barChart/barchart.js
+++ src/components/barChart/barchart.js
@@ -5,16 +5,27 @@ export default StyleSheet.create({
     'position': 'relative',
     'maxWidth': [{ 'unit': 'px', 'value': 400 }],
     'margin': [{ 'unit': 'px', 'value': 60 }, { 'unit': 'string', 'value': 'auto' }, { 'unit': 'px', 'value': 60 }, { 'unit': 'string', 'value': 'auto' }],
-    'maxHeight': [{ 'unit': 'px', 'value': 250 }],
-    'minHeight': [{ 'unit': 'px', 'value': 250 }],
-    'height': [{ 'unit': 'px', 'value': 250 }]
+    'height': [{ 'unit': 'px', 'value': 200 }],
+    'maxHeight': [{ 'unit': 'px', 'value': 200 }],
+    'minHeight': [{ 'unit': 'px', 'value': 200 }],
+    // overflow: hidden;
+    'display': 'flex',
+    'flexWrap': 'wrap',
+    'alignItems': 'flex-end'
+  },
+  'barchart__title': {
+    'width': [{ 'unit': '%H', 'value': 1 }]
   },
   'barchart__bar': {
-    'position': 'absolute',
+    'minHeight': [{ 'unit': 'px', 'value': 35 }],
+    // position: absolute;
     'bottom': [{ 'unit': 'px', 'value': 0 }],
-    'width': [{ 'unit': 'px', 'value': 190 }],
+    'width': [{ 'unit': '%H', 'value': 0.5 }],
+    'boxSizing': 'border-box',
     'paddingTop': [{ 'unit': 'px', 'value': 10 }],
     'paddingBottom': [{ 'unit': 'px', 'value': 10 }],
+    'WebkitTransition': 'height 0.5s',
+    'transition': 'height 0.5s',
     'background': '#3f51b5',
     'background': '-moz-linear-gradient(top, #3f51b5 0%, #2e44b2 100%)',
     'background': '-webkit-linear-gradient(top, #3f51b5 0%,#2e44b2 100%)',
@@ -23,6 +34,7 @@ export default StyleSheet.create({
     'color': 'white'
   },
   'barchart__bar:last-of-type': {
-    'right': [{ 'unit': 'px', 'value': 0 }]
+    'right': [{ 'unit': 'px', 'value': 0 }],
+    'float': 'right'
   }
 });
